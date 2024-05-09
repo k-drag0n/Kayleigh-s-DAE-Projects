28956f36fe0e4bf798f49c9a8c127988db39f520
k-drag0n <kayleigh@kirchoff.com>
2024-05-09 17:27:24 -0400

    starting to add art assets now

--- godot stuff/CharacterBody2D.gd
+++ godot stuff/CharacterBody2D.gd
@@ -6,16 +6,24 @@ const GRAVITY = 600
 
 @export var speed = 200.0
 
-@export var jumpstrength := -400.0
+@export var jumpstrength := -300.0
 @export var maxjumps := 2
 @export var jumps_made = 0
-@export var doublejumpstrength :=  -50.0
+@export var doublejumpstrength :=  -250.0
+
+
 
 
 var _jumpsmade := 0
 var _velocity := Vector2.ZERO
 
+
 func _physics_process(delta):
+	
+	var is_falling = _velocity.y > 0.0 and not is_on_floor()
+	var is_jumping = Input.is_action_just_pressed("jump") and is_on_floor()
+	var is_doublejumping = Input.is_action_just_pressed("jump") and is_falling
+	
 	velocity.y += delta * GRAVITY
 	
 	var motion = velocity * delta 
@@ -30,11 +38,16 @@ func _physics_process(delta):
 	else:
 		velocity.x = 0.0
 	
-	if is_on_floor():
-		if Input.is_action_just_pressed("jump"):
-			print("jumped")
+			
+	if Input.is_action_just_pressed("jump"):
+		if is_falling:
+			print("double jumped")
+			velocity.y = doublejumpstrength
+		else:
 			velocity.y = jumpstrength
+			print("jumped")
+			jumps_made = 1 + jumps_made
 
-	var is_falling = _velocity.y > 0.0 and not is_on_floor()
-	var is_jumping = Input.is_action_just_pressed("jump") and is_on_floor()
-	var is_doublejumping = Input.is_action_just_pressed("jump") and is_falling
+
+	if Input.is_action_just_pressed("jump"):
+		print(jumps_made)
